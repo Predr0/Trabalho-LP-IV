f96bf7cd6cea5d450a4731cceb7dc810ff551bb9
Predr0 <pedro.magalhaes05@hotmail.com>
2025-10-06 21:08:41 -0300

    teste adicionado

--- src/test/java/com/example/imc/CalculaIMCTest.java
+++ src/test/java/com/example/imc/CalculaIMCTest.java
@@ -1,4 +1,49 @@
-package test.java.com.example.imc;
+import com.example.imc.Individuo;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-public class CalculaIMCTest {
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculaIMCTest {
+
+    Individuo individuo;
+
+    @BeforeEach
+    void setUp() {
+        individuo = new Individuo();
+        individuo.setAltura(1.0f);
+    }
+
+    @Test
+    void deveRetornarMasculinoAbaixoPeso() {
+        individuo.setPeso(20.6f);
+        individuo.setGenero("masculino");
+        assertEquals("Abaixo do peso", individuo.definirStatusImc());
+    }
+
+
+    @Test
+    void deveRetornarMasculinoPesoNormal() {
+        individuo.setPeso(26.3f);
+        individuo.setGenero("masculino");
+        assertEquals("Peso normal", individuo.definirStatusImc());
+    }
+
+
+    @Test
+    void deveVerificarPesoValido() {
+        individuo.setPeso(0.1f);
+        assertEquals(0.1f, individuo.getPeso());
+    }
+
+    @Test
+    void deveVerificarPesoInvalido() {
+        try {
+            individuo.setPeso(0.0f);
+            fail();
+        }
+        catch (IllegalArgumentException e) {
+            assertEquals("peso invalido", e.getMessage());
+        }
+    }
 }
